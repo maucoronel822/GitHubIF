f1ec3ccb0dcbc59945b36b784078a3bc4ddbcaa9
Mauricio Coronel León <mauricio2405049@hybridge.education>
2025-01-22 07:38:50 -0600

    Add files via upload

---
+++ Ciclo_for_listas.py
@@ -0,0 +1,17 @@
+personas = [
+    ["Gerardo", "Mathus", 30, "profesor"]
+,
+    ["Sahori", "Solana", 24, "tutora"]
+,
+    ["Iván", "Martínez", 25, "desarrolador"]
+
+,
+    ["José", "Esteva", 24, "desarrollador"]
+]
+
+#for persona in personas:
+ #   print(f"Hola, tienes {persona[0]} {persona[1]}. Tienes {persona[2]} años, y eres {persona[3]}.")
+
+for i in range(len(personas)):
+    print(f"Hola, tienes {personas[i][0]} {personas[i][1]}. Tienes {personas[i][2]} años, y eres {personas[i][3]}.")
+    #print(saludo)
